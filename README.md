2d3a0d7fd4267ad0c298e0cab78e65038b3b4076
Ryu Woosik <ryuwoosil@gmail.com>
2025-07-29 22:38:41 +0900

    Feat: Build Structure

---
+++ README.md
@@ -0,0 +1,51 @@
+<h4 align="center">MUSTOM, More Than Custom</h4>
+
+### About This Library
+This library is specifically designed for the Mustom project. Therefore, it's not a general-purpose validation library and might not be suitable for your project.
+
+### How to Use
+
+# Single
+
+```
+# Validation without option
+# It will validate if input value is a natural numer, not empty, and required.
+validator.single().naturalNumber().notEmpty().required()
+
+# Validation with options
+validator.single({}).string()
+
+```
+
+# Iterable (Object, Array of Object)
+
+
+
+
+# Options
+parseMode
+
+'definedOnly',
+
+
+returnValue
+
+
+
+
+# Custom Error Object
+
+### License
+
+This library is under AGPL v3 Licensed. (Same as the license of Mustom)
+
+
+### Git Repository
+[GitHub] (https://github.com/dddddd)
+
+### Project website
+
+[MUSTOM HQ] (https://mustom.com/)
+
+
+
---
+++ custom-error.js
@@ -0,0 +1,48 @@
+// MUSTOM, More Than Custom, https://mustom.com
+// Copyright © Ryu Woosik. All rights reserved.
+
+class BaseError extends Error {
+    constructor(code, message, options = {}) {
+        super(message)        
+
+        if (Error.captureStackTrace) {
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+        this.name = this.constructor.name
+        this.code = code
+        this.message = message
+
+        if (options.details) {
+            this.details = options.details
+        }
+
+        if (options.userMessage) {
+            this.userMessage = options.userMessage
+        }
+        
+        if (options.timestamp) {
+            this.timestamp = options.timestamp
+        }
+    }
+}
+
+class DataTypeError extends BaseError {
+    constructor(code, message, options = {}) {
+        super(code, message, options)
+        this.name = 'ValidationError'
+    }
+}
+
+class EmptyValueError extends BaseError {
+    constructor(code, message, options = {}) {
+        super(code, message, options)
+        this.name = 'ValidationError'
+    }
+}
+
+module.exports = { 
+    BaseError, 
+    DataTypeError, 
+    EmptyValueError
+ }
